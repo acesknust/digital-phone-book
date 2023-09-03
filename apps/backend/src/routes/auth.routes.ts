66c51a9223c458843f7c4a6b2803e7f75ee85ac8
Kwameowusu <daarich470@gmail.com>
2023-09-03 12:55:10 +0000

    feat: yarn workspace

---
+++ apps/backend/src/routes/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as controller from "../controllers/auth.contoller";
+
+const router = Router()
+
+router.post('/signup', controller.signup)
+router.post('/signin', controller.signin)
+
+export default router
